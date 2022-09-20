292c22c7bf819f70e94130feba3363d551929c22
hwayounj <hwayounj@andrew.cmu.edu>
2022-09-20 19:19:38 -0400

    9/20

---
+++ solution-hw3/js/app.js
@@ -0,0 +1,38 @@
+// 1. glazing options Object
+
+const glazingOptions = {
+    keepOriginal: 0,
+    sugarMilk: 0,
+    vanilaMilk: 5,
+    doubleChocolate: 1.5
+}
+
+// 2. pact options Object
+const packSize = {
+    1 :  1,
+    3 : 3,
+    6 : 5,
+    12: 10
+}
+
+// populate the detail page with the options above
+const glazingSelectionElement = document.querySelector('option.value');
+
+//append options to this selection by looping through glazing options object
+//create option element and append it to the selection object
+
+//repeat this for the pack size options
+
+
+console.log(glazingSelectionElement);
+
+
+
+// find what has been selected as glazing options
+
+
+// find what has been selected as pack options
+
+// 3.find java script object that will be updated via javascript
+
+// 4.compute the change based on the selection
