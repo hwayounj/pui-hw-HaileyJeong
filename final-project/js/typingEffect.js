6f187605e1483f6189a98f75d923cec8c24c405b
hwayounj <90377670+hwayounj@users.noreply.github.com>
2022-12-10 09:07:19 -0500

    Add files via upload
    project

---
+++ final-project/js/typingEffect.js
@@ -0,0 +1,20 @@
+// npm install typed.js
+
+// import Typed from 'typed.js';
+
+
+// <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
+
+let typing=new Typed(".typing",{
+    strings:["Hailey","Artist","Designer"],
+    backSpeed:80,
+    typeSpeed:100,
+    backDelay:1500,
+    loop: true
+});
+
+// var typed = new Typed('#typed', {
+//     stringsElement: '#typed-strings'
+//   });
+
+
